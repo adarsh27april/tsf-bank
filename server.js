4fadbb756ed651bb9bb72ee9eab338b79a45a6df
Adarsh Kumar Singh <54088735+adarsh27april@users.noreply.github.com>
2021-06-17 13:36:53 +0530

    Update server.js
    added port variable

--- server.js
+++ server.js
@@ -111,7 +111,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
             res.redirect('/');
         });
 
-        app.listen(3000, function () {
-            console.log('listening on 3000')
+    const port = process.env.PORT || 3000    
+    app.listen(port, function () {
+            console.log(`listening on ${port}`)
         });
     });
